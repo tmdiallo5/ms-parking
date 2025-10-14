f8566d395ef985b0ba39db1142074c70f3e4c8ba
Thierno Maviatou Diallo <tmdiallo5@gmail.com>
2025-10-14 23:39:48 +0100

    RSA Key generation

--- src/main/java/com/parking/ms_parking/MsParkingApplication.java
+++ src/main/java/com/parking/ms_parking/MsParkingApplication.java
@@ -1,10 +1,16 @@
 package com.parking.ms_parking;
 
+
+import com.parking.ms_parking.security.RsaKeys;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 
+
+@EnableConfigurationProperties(RsaKeys.class)
 @SpringBootApplication(
 		exclude = {
 				SecurityAutoConfiguration.class,
